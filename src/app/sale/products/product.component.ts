bfad02b1b24cea04ac254f54c2fef1d465f925c0
filipeoliveira <filipe.oliveira@picpay.com>
2020-07-19 21:37:19 -0300

    Definindo configurações base do projeto e criando tela inicial

---
+++ src/app/sale/products/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'baas-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css']
+})
+  
+export class ProductComponent {
+    description = "PIZZA BLOCOLIS"
+}
---
+++ src/app/sale/sale.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { ProductComponent } from './products/product.component';
+
+@NgModule({
+    declarations: [
+        ProductComponent
+    ]
+})
+export class SaleModule { }
